9b1c1170c3e81c441fae496af962b6d4cd79b74d
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-05-25 19:42:42 -0700

    export ObjectiveFn type from optimizers package

--- src/staliro/optimizers/__init__.py
+++ src/staliro/optimizers/__init__.py
@@ -1,4 +1,4 @@
 from .dual_annealing import DualAnnealing
-from .optimizer import Optimizer
+from .optimizer import Optimizer, ObjectiveFn
 from .uniform_random import UniformRandom
 from .basinhopping import Basinhopping
