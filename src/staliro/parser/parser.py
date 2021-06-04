fae3d9f59d0f2f5e1756ba7316561f221da9b524
Jacob Anderson <andersonjwan@outlook.com>
2021-06-04 16:34:18 -0700

    removed unnecessary comments

--- src/staliro/parser/parser.py
+++ src/staliro/parser/parser.py
@@ -30,18 +30,13 @@ def parse(formula: str, predicates: Predicates, mode: str = "cpu") -> Optional[S
     from .stlParser import stlParser as Parser
     from .stlParserVisitor import stlParserVisitor as Visitor
 
-    # convert string to ANTLRv4 InputStream
     input_stream = InputStream(formula)
 
-    # create a token stream from the STPL lexer
     lexer = Lexer(input_stream)
     stream = CommonTokenStream(lexer)
 
-    # return a ParseTree from the STPL parser
     parser = Parser(stream)
     tree = parser.stlSpecification()
-
-    # visit the tree and generate equivalent MTL from formula
     visitor = Visitor(lexer, predicates, mode)
 
     return visitor.visit(tree)  # type: ignore
