54cbbb05fd13308d026cff3c04f89ef69818c3f8
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-06-08 16:54:16 -0700

    export Iteration and Run from optimizers package

--- src/staliro/optimizers/__init__.py
+++ src/staliro/optimizers/__init__.py
@@ -1,4 +1,4 @@
 from .dual_annealing import DualAnnealing
-from .optimizer import Optimizer, ObjectiveFn
+from .optimizer import Optimizer, ObjectiveFn, Iteration, Run
 from .uniform_random import UniformRandom
 from .basinhopping import Basinhopping
