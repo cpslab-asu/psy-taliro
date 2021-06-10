dec692345ee4df293fafe8f1d4f758ab243448a5
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-06-10 10:02:13 -0700

    Create Observation and Falsification classes for users to indicate when a sample should be a failure Privatize Blackbox and ODE models to avoid construction outside of decorators

--- src/staliro/models.py
+++ src/staliro/models.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
 import sys
+from dataclasses import dataclass
 from math import floor
-from typing import Callable, Sequence, Type, Tuple, Union
+from typing import Tuple, Union
 
-if sys.version_info > (3, 8):
+if sys.version_info >= (3, 9):
+    from collections.abc import Sequence, Callable
+else:
+    from typing import Sequence, Callable
+
+if sys.version_info >= (3, 8):
     from typing import Protocol, runtime_checkable
 else:
     from typing_extensions import Protocol, runtime_checkable
@@ -15,9 +21,44 @@ from scipy import integrate
 from .options import Interval
 from .signals import SignalInterpolator
 
-ModelResult = Tuple[ndarray, ndarray]
+
+@dataclass(frozen=True)
+class Observations:
+    _trajectories: ndarray
+    _timestamps: ndarray
+
+    def __post_init__(self) -> None:
+        if self._timestamps.ndim != 1:
+            raise ValueError("timestamps must be 1-dimensional")
+
+        if self._trajectories.ndim != 2:
+            raise ValueError("trajectories must be 2-dimensional")
+
+        times_len = self._timestamps.shape[0]
+        traj_shape = self._trajectories.shape
+
+        if traj_shape[0] != times_len and traj_shape[1] != times_len:
+            raise ValueError("trajectories must have one axis of equal length to timestamps")
+
+    @property
+    def timestamps(self) -> ndarray:
+        return self._timestamps
+
+    @property
+    def trajectories(self) -> ndarray:
+        if self._trajectories.shape[0] == self._timestamps.shape[0]:
+            return self._trajectories.T
+
+        return self._trajectories
+
+
+class Falsification:
+    pass
+
+
 StaticParameters = ndarray
 SignalInterpolators = Sequence[SignalInterpolator]
+ModelResult = Union[Observations, Falsification]
 
 
 @runtime_checkable
@@ -35,11 +76,18 @@ SignalTimes = ndarray
 SignalValues = ndarray
 Timestamps = Union[ndarray, Sequence[float]]
 Trajectories = Union[ndarray, Sequence[Sequence[float]]]
-BlackboxResult = Tuple[Trajectories, Timestamps]
+BlackboxResult = Union[Observations, Falsification, Tuple[Trajectories, Timestamps]]
 BlackboxFunc = Callable[[StaticParameters, SignalTimes, SignalValues], BlackboxResult]
 
 
-class Blackbox(Model):
+def _make_observation(result: Tuple[Trajectories, Timestamps]) -> Observations:
+    if len(result) != 2:
+        raise ValueError("Expected tuple to be length 2")
+
+    return Observations(array(result[0]), array(result[1]))
+
+
+class _Blackbox(Model):
     def __init__(self, func: BlackboxFunc, sampling_interval: float = 0.1):
         self.func = func
         self.sampling_interval = sampling_interval
@@ -54,9 +102,14 @@ class Blackbox(Model):
         point_count = floor(duration / self.sampling_interval)
         signal_times = linspace(start=interval.lower, stop=interval.upper, num=point_count)
         signal_traces = [interpolator.interpolate(signal_times) for interpolator in interpolators]
-        trajectories, timestamps = self.func(static_params, signal_times, array(signal_traces))
+        result = self.func(static_params, signal_times, array(signal_traces))
 
-        return array(trajectories), array(timestamps)
+        if isinstance(result, tuple):
+            return _make_observation(result)
+        elif isinstance(result, (Observations, Falsification)):
+            return result
+        else:
+            raise ValueError("Unknown return type from blackbox")
 
 
 Time = float
@@ -76,7 +129,7 @@ def _make_integration_fn(signals: SignalInterpolators, func: ODEFunc) -> Integra
     return integration_fn
 
 
-class ODE(Model):
+class _ODE(Model):
     def __init__(self, func: ODEFunc):
         self.func = func
 
@@ -92,12 +145,9 @@ class ODE(Model):
         return integration.y, integration.t.astype(float32)
 
 
-BlackboxFactory = Callable[[BlackboxFunc], Blackbox]
+def blackbox(*, sampling_interval: float = 0.1) -> Callable[[BlackboxFunc], _Blackbox]:
+    return lambda f: _Blackbox(f, sampling_interval)
 
 
-def blackbox(*, sampling_interval: float = 0.1) -> BlackboxFactory:
-    return lambda f: Blackbox(f, sampling_interval)
-
-
-def ode() -> Type[ODE]:
-    return ODE
+def ode() -> Callable[[ODEFunc], _ODE]:
+    return _ODE
