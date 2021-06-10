173439cd0597431cbd7dfce7269d00a0b662d666
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-06-10 13:05:33 -0700

    Test both ways of using blackbox decorator

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,12 +11,22 @@ from staliro.models import (
 
 
 class ModelTestCase(TestCase):
-    def test_blackbox_decorator(self) -> None:
+    def test_blackbox_decorator_args(self) -> None:
         @blackbox(sampling_interval=0.2)
-        def func1(
+        def dummy(
             params: StaticParameters, times: SignalTimes, signals: SignalValues
         ) -> BlackboxResult:
             pass
 
-        self.assertIsInstance(func1, _Blackbox)
-        self.assertEqual(func1.sampling_interval, 0.2)
+        self.assertIsInstance(dummy, _Blackbox)
+        self.assertEqual(dummy.sampling_interval, 0.2)
+
+    def test_blackbox_decorator_no_args(self) -> None:
+        @blackbox
+        def dummy(
+            params: StaticParameters, times: SignalTimes, signals: SignalValues
+        ) -> BlackboxResult:
+            pass
+
+        self.assertIsInstance(dummy, _Blackbox)
+        self.assertEqual(dummy.sampling_interval, 0.1)
