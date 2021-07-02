5dc6f27e940ede1f48fcc3f391aa866c31fd7b4a
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-02 16:10:37 -0700

    refactor: Update main package exports

--- src/staliro/__init__.py
+++ src/staliro/__init__.py
@@ -1,1 +1,5 @@
-from .staliro import staliro
+from .staliro import staliro, staliro_timed
+from .options import StaliroOptions as Options, SignalOptions
+from .specification import TLTK, RTAMTDense, RTAMTDiscrete, PredicateProps
+from .models import blackbox, ode
+from .optimizers import UniformRandom, DualAnnealing
