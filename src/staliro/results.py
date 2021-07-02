134bebbdb919949deb0d86cfcb364910d51e6f21
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-02 15:09:43 -0700

    feat(results): Create Run and Iteration classes in results module.
    Results are consumer-facing classes that should not be manipulable by user-created classes. A run and result are both generic in the type of the result attribute and the type of the elements of the history attribute

--- src/staliro/results.py
+++ src/staliro/results.py
@@ -1,23 +1,43 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from datetime import timedelta
-from typing import Sequence
+from typing import Sequence, Generic, TypeVar
 
-from .optimizers import Run
-
-EMPTY_TIMEDELTA = timedelta(0)
+from .optimizers import OptimizerResult, Sample
 
 
-@dataclass
-class StaliroResult:
-    runs: Sequence[Run]
+@dataclass(frozen=True)
+class Iteration:
+    cost: float
+    sample: Sample
+
+
+@dataclass(frozen=True)
+class TimedIteration(Iteration):
+    model_duration: float
+    cost_duration: float
+
+
+_RT = TypeVar("_RT", bound=OptimizerResult)
+_IT = TypeVar("_IT", bound=Iteration)
+
+
+@dataclass(frozen=True)
+class Run(Generic[_RT, _IT]):
+    result: _RT
+    history: Sequence[_IT]
+    duration: float
+
+    @property
+    def best_iter(self) -> _IT:
+        return min(self.history, key=lambda i: i.cost)
+
+
+@dataclass(frozen=True)
+class StaliroResult(Generic[_RT, _IT]):
+    runs: Sequence[Run[_RT, _IT]]
     seed: int
 
     @property
-    def best_run(self) -> Run:
-        return min(self.runs, key=lambda r: r.best_iter.robustness)
-
-    @property
-    def run_time(self) -> timedelta:
-        return sum((run.run_time for run in self.runs), EMPTY_TIMEDELTA)
+    def best_run(self) -> Run[_RT, _IT]:
+        return min(self.runs, key=lambda r: r.best_iter.cost)
