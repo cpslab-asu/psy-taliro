876352a4a6a82fd0796b5cd69faf6325fdc4fb11
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-02 15:53:05 -0700

    refactor(optimizers): Update uniform random optimizer to latest interface

--- src/staliro/optimizers/uniform_random.py
+++ src/staliro/optimizers/uniform_random.py
@@ -1,38 +1,28 @@
 from __future__ import annotations
 
-from datetime import datetime
-from typing import Iterable, Sequence
+from typing import Sequence
 
-from numpy import array, ndarray
+from numpy import array, float_
+from numpy.typing import NDArray
 from numpy.random import default_rng, Generator
 
-from .optimizer import ObjectiveFn, Optimizer, Iteration, Run, RunOptions
+from .optimizer import ObjectiveFn, Optimizer, OptimizerResult, RunOptions
 from ..options import Interval, Behavior
 
-_Samples = Sequence[ndarray]
-_Bounds = Sequence[Interval]
-_Iterations = Iterable[Iteration]
 
-
-def _sample(bounds: _Bounds, rng: Generator) -> ndarray:
+def _sample(bounds: Sequence[Interval], rng: Generator) -> NDArray[float_]:
     return array([rng.uniform(bound.lower, bound.upper) for bound in bounds])
 
 
-def _iterations(samples: _Samples, func: ObjectiveFn, behavior: Behavior) -> _Iterations:
-    for sample in samples:
-        robustness = func(sample)
-
-        yield Iteration(robustness, sample)
-
-        if behavior is Behavior.FALSIFICATION and robustness < 0:
-            break
-
-
-class UniformRandom(Optimizer[Run]):
-    def optimize(self, func: ObjectiveFn, options: RunOptions) -> Run:
-        start_time = datetime.now()
+class UniformRandom(Optimizer[OptimizerResult]):
+    def optimize(self, func: ObjectiveFn, options: RunOptions) -> OptimizerResult:
         rng = default_rng(options.seed)
         samples = [_sample(options.bounds, rng) for _ in range(options.iterations)]
-        history = list(_iterations(samples, func, options.behavior))
 
-        return Run(history, datetime.now() - start_time)
+        for sample in samples:
+            cost = func(sample)
+
+            if options.behavior is Behavior.FALSIFICATION and cost < 0:
+                break
+
+        return OptimizerResult()
