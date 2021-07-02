03095fc02c3503ba0fe0e1a86c70c86af64888b1
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-02 15:53:55 -0700

    refactor(optimizers): Update basinhopping optimizer stub to latest interface

--- src/staliro/optimizers/basinhopping.py
+++ src/staliro/optimizers/basinhopping.py
@@ -1,6 +1,6 @@
-from .optimizer import Optimizer, ObjectiveFn, Run, RunOptions
+from .optimizer import Optimizer, ObjectiveFn, RunOptions, OptimizerResult
 
 
-class Basinhopping(Optimizer[Run]):
-    def optimize(self, func: ObjectiveFn, options: RunOptions) -> Run:
+class Basinhopping(Optimizer[OptimizerResult]):
+    def optimize(self, func: ObjectiveFn, options: RunOptions) -> OptimizerResult:
         raise NotImplementedError()
