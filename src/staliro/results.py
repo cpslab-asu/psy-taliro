35ad29b708ff8aa2a4a67155aab455106c18e674
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-08 12:58:40 -0700

    feat(results): Create timed variant of Run class to expose timing stats as properties

--- src/staliro/results.py
+++ src/staliro/results.py
@@ -33,6 +33,28 @@ class Run(Generic[_RT, _IT]):
         return min(self.history, key=lambda i: i.cost)
 
 
+_TIT = TypeVar("_TIT", bound=TimedIteration)
+
+
+@dataclass(frozen=True)
+class TimedRun(Run[_RT, _TIT]):
+    @property
+    def avg_model_duration(self) -> float:
+        return mean(iteration.model_duration for iteration in self.history)
+
+    @property
+    def total_model_time(self) -> float:
+        return sum(iteration.model_duration for iteration in self.history)
+
+    @property
+    def avg_cost_duration(self) -> float:
+        return mean(iteration.cost_duration for iteration in self.history)
+
+    @property
+    def total_cost_time(self) -> float:
+        return sum(iteration.cost_duration for iteration in self.history)
+
+
 @dataclass(frozen=True)
 class StaliroResult(Generic[_RT, _IT]):
     runs: Sequence[Run[_RT, _IT]]
