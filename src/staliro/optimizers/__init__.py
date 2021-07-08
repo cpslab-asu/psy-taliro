56ad9ffdf03ecb7a1a88b4f8213de3a4cb127376
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-07-08 12:52:09 -0700

    refactor(optimizers): Remove OptimizerResult export from optimizers package

--- src/staliro/optimizers/__init__.py
+++ src/staliro/optimizers/__init__.py
@@ -1,4 +1,4 @@
 from .dual_annealing import DualAnnealing
-from .optimizer import Optimizer, Sample, ObjectiveFn, RunOptions, OptimizerResult
+from .optimizer import Optimizer, Sample, ObjectiveFn, RunOptions
 from .uniform_random import UniformRandom
 from .basinhopping import Basinhopping
