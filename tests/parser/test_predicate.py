c4abc8ecf79356b873e2c52c202196b17f0a6adc
Jacob Anderson <andersonjwan@outlook.com>
2021-07-27 23:25:28 -0700

    style(tests): Simplify parser predicate test names

--- tests/parser/test_predicate.py
+++ tests/parser/test_predicate.py
@@ -14,31 +14,31 @@ class PredicateTestCase(ParserTestCase):
         self.assertEqual(predicate.A_Matrix, expected.A_Matrix)
         self.assertEqual(predicate.bound, expected.bound)
 
-    def test_predicate_positive_ns_less_than_positive(self) -> None:
+    def test_predicate_pos_ns_less_than_pos(self) -> None:
         self._do_test(r"pred4 <= 1.0", self._preds["pred4"])
 
-    def test_predicate_positive_ns_less_than_negative(self) -> None:
+    def test_predicate_pos_ns_less_than_neg(self) -> None:
         self._do_test(r"pred5 <= -1.0", self._preds["pred5"])
 
-    def test_predicate_negative_ns_less_than_positive(self) -> None:
+    def test_predicate_neg_ns_less_than_pos(self) -> None:
         self._do_test(r"-pred6 <= 1.0", self._preds["pred6"])
 
-    def test_predicate_negative_ns_less_than_negative(self) -> None:
+    def test_predicate_neg_ns_less_than_neg(self) -> None:
         self._do_test(r"-pred7 <= -1.0", self._preds["pred7"])
 
-    def test_predicate_positive_ns_greater_than_positive(self) -> None:
+    def test_predicate_pos_ns_greater_than_pos(self) -> None:
         self._do_test(r"pred7 >= 1.0", self._preds["pred7"])
 
-    def test_predicate_positive_ns_greater_than_negative(self) -> None:
+    def test_predicate_pos_ns_greater_than_neg(self) -> None:
         self._do_test(r"pred6 >= -1.0", self._preds["pred6"])
 
-    def test_predicate_negative_ns_greater_than_positive(self) -> None:
+    def test_predicate_neg_ns_greater_than_pos(self) -> None:
         self._do_test(r"-pred5 >= 1.0", self._preds["pred5"])
 
-    def test_predicate_negative_ns_greater_than_negative(self) -> None:
+    def test_predicate_neg_ns_greater_than_neg(self) -> None:
         self._do_test(r"-pred4 >= -1.0", self._preds["pred4"])
 
-    def test_predicate_positive_ns_less_than_positive_scientific(self) -> None:
+    def test_predicate_pos_ns_less_than_pos_scientific_pos(self) -> None:
         self._do_test(r"pred8 <= 1.1e5", self._preds["pred8"])
 
     def test_predicate_pos_ns_less_than_pos_scientific_neg(self) -> None:
