5ad35d796758ddf77751a8c39c638204ba465630
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-08-16 10:16:37 -0700

    build: Update sphinx configuration file

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import importlib.metadata as metadata
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -17,12 +19,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'pystaliro'
-copyright = '2021, Georgios Fainekos'
-author = 'Georgios Fainekos'
+project = "PSY-TaLiRo"
+copyright = "2021, Georgios Fainekos"
+author = "Georgios Fainekos"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = metadata.version("psy-taliro")
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +32,15 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +48,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
