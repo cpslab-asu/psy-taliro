994f2f889a6e8423968a03e1e4c32b256763da67
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-09-03 08:42:14 -0700

    fix: Remove exports from staliro package that were moved to the staliro.core package

--- src/staliro/__init__.py
+++
@@ -1,6 +0,0 @@
-from .staliro import staliro
-from .options import Options, SignalOptions, Behavior
-from .scenarios import Scenario
-from .specification import TLTK, RTAMTDense, RTAMTDiscrete, PredicateProps
-from .models import blackbox, ode
-from .optimizers import UniformRandom, DualAnnealing
