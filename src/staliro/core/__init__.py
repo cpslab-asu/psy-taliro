b23fd0153ec5ef9461995a8b91542120961e8861
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-09-03 08:41:02 -0700

    feat: Make core directory as package. Export data types from core modules

---
+++ src/staliro/core/__init__.py
@@ -0,0 +1,7 @@
+from .interval import Interval
+from .model import Model, SystemInputs, SystemData, SystemFailure
+from .optimizer import Optimizer, ObjectiveFn, OptimizationParams, Behavior
+from .result import Result
+from .sample import Sample
+from .signal import Signal
+from .specification import Specification
