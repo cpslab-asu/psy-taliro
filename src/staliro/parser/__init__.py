90c0e3db2b59e99cac3e946091c48aab61d99b90
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-10-13 15:43:10 -0700

    feat: Include __all__ array in parser package init module
    Remove extraneous, unneeded imports.

--- src/staliro/parser/__init__.py
+++ src/staliro/parser/__init__.py
@@ -1,7 +1,3 @@
-from typing import TYPE_CHECKING
-
 from .parser import parse
 
-if TYPE_CHECKING:
-    from .parser import Predicates
-    from .stlSpecification import StlSpecification
+__all__ = ["parse"]
