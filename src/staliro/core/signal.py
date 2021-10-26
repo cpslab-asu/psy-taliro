bfef12a94207a7f9643fb7f6fe77c2a4efc72817
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-10-26 16:27:07 -0700

    feat: Enable runtime checking for SignalFactory protocol

--- src/staliro/core/signal.py
+++ src/staliro/core/signal.py
@@ -7,13 +7,20 @@ from typing_extensions import Protocol, runtime_checkable
 
 @runtime_checkable
 class Signal(Protocol):
+    """Representation of a time-varying input to a system."""
+
     def at_time(self, time: float) -> float:
+        """Get the value of the signal at the specified time."""
         ...
 
     def at_times(self, times: Sequence[float]) -> List[float]:
+        """Get the value of the signal at each specified time."""
         ...
 
 
+@runtime_checkable
 class SignalFactory(Protocol):
+    """Construct a Signal using the provided times and signal values."""
+
     def __call__(self, __times: Sequence[float], __values: Sequence[float]) -> Signal:
         ...
