a08db9856a11280837f5a1b5835a23c01c046569
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-10-27 10:30:08 -0700

    refactor: Remove conditional feature checking from parser module

--- src/staliro/parser/parser.py
+++ src/staliro/parser/parser.py
@@ -2,31 +2,27 @@ from __future__ import annotations
 
 from typing import Dict, Optional, Union, Sequence
 
+import tltk_mtl as mtl
 from antlr4.CommonTokenStream import CommonTokenStream
 from antlr4.InputStream import InputStream
 
-try:
-    import tltk_mtl as mtl
-except ImportError:
-    _has_tltk = False
-else:
-    _has_tltk = True
-    PredicateDict = Dict[str, mtl.Predicate]
-    Predicates = Union[Sequence[str], PredicateDict]
-    StlSpecification = Union[
-        mtl.And,
-        mtl.Finally,
-        mtl.Global,
-        mtl.Implication,
-        mtl.Next,
-        mtl.Not,
-        mtl.Or,
-        mtl.Predicate,
-        mtl.Until,
-    ]
+PredicateDict = Dict[str, mtl.Predicate]
+PredicateNameSeq = Sequence[str]
+Predicates = Union[PredicateNameSeq, PredicateDict]
+TltkObject = Union[
+    mtl.And,
+    mtl.Finally,
+    mtl.Global,
+    mtl.Implication,
+    mtl.Next,
+    mtl.Not,
+    mtl.Or,
+    mtl.Predicate,
+    mtl.Until,
+]
 
 
-def parse(formula: str, predicates: Predicates, mode: str = "cpu") -> Optional[StlSpecification]:
+def parse(formula: str, predicates: Predicates, mode: str = "cpu") -> Optional[TltkObject]:
     """TLTk parser parses a specification requirement into an equivalent TLTk structure
 
     Attributes:
@@ -34,9 +30,6 @@ def parse(formula: str, predicates: Predicates, mode: str = "cpu") -> Optional[S
         predicates: The set of Predicate(s) used in the requirement
         mode: The TLTk computation mode
     """
-
-    if not _has_tltk:
-        raise RuntimeError("TLTK must be installed to use parser functionality")
 
     from .stlLexer import stlLexer as Lexer
     from .stlParser import stlParser as Parser
