57cdd9b9231b523073e4fb9cd215d3dda53152a9
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-10-28 09:12:02 -0700

    test: Update tests for interval class

--- tests/core/test_interval.py
+++ tests/core/test_interval.py
@@ -6,28 +6,31 @@ from staliro.core.interval import Interval
 
 
 class IntervalTestCase(TestCase):
-    def test_arg_types(self) -> None:
-        Interval(1, 2)
-        Interval(1.0, 2.0)
-        Interval(1, 2.0)
-        Interval(1.0, 2)
+    def test_int_bound(self) -> None:
+        interval = Interval(0, 1)
 
+        self.assertIsInstance(interval.lower, float)
+        self.assertIsInstance(interval.upper, float)
+
+    def test_float_bound(self) -> None:
+        interval = Interval(0.0, 1.0)
+
+        self.assertIsInstance(interval.lower, float)
+        self.assertIsInstance(interval.upper, float)
+
+    def test_nonnumeric_bound(self) -> None:
         with self.assertRaises(TypeError):
-            Interval("a", "b")  # type: ignore
-            Interval([1, 2], 3)  # type: ignore
-            Interval(2, {1, 2})  # type: ignore
-            Interval(["a", "b"])  # type: ignore
-            Interval({"a": 1, "b": 2})  # type: ignore
-            Interval(["a", "b"], "c")  # type: ignore
-            Interval("a", ["b", "c"])  # type: ignore
+            Interval([1, 2], "b")  # type: ignore
 
-    def test_arg_order(self) -> None:
+    def test_bounds_order(self) -> None:
         with self.assertRaises(ValueError):
-            Interval(2, 1)
+            Interval(1, 1)
+            Interval(1, 0)
 
-    def test_attributes(self) -> None:
-        interval = Interval(1, 2.0)
+    def test_tuple_conversion(self) -> None:
+        interval = Interval(0, 1)
+        astuple = interval.astuple()
 
-        self.assertEqual(interval.lower, 1.0)
-        self.assertEqual(interval.upper, 2.0)
-        self.assertEqual(interval.length, 1.0)
+        self.assertIsInstance(astuple, tuple)
+        self.assertEqual(astuple[0], interval.lower)
+        self.assertEqual(astuple[1], interval.upper)
