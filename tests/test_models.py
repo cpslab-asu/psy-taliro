517554bc096e427d2c19061d0caa3b952a3bdcdb
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-10-28 10:47:20 -0700

    test: Update model test cases
    Create test case to verify blackbox simulate method performs correctly. Create test case for ode decorator. ODE model test case not implemented because there is little logic in the model aside from the execution of the SciPy ivp function

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,65 +1,84 @@
 from typing import Any
 from unittest import TestCase
+from unittest.mock import Mock, NonCallableMock
 
-import numpy as np
-import staliro.models as models
+from numpy import ndarray
+from numpy.typing import NDArray
+from staliro.core.interval import Interval
+from staliro.core.model import ModelData
+from staliro.core.signal import Signal
+from staliro.models import Blackbox, ODE, StaticInput, SignalTimes, SignalValues, State
+from staliro.models import blackbox, ode
+
+AnyData = ModelData[Any, Any]
+
+
+class BlackboxTestCase(TestCase):
+    def test_simulation(self) -> None:
+        sampling_interval = 0.1
+        func = Mock(return_value=NonCallableMock(spec=ModelData))
+        model: Blackbox[Any, Any] = Blackbox(func, sampling_interval=sampling_interval)
+
+        static_inputs = [1, 2, 3, 4]
+        signals = [Mock(spec=Signal), Mock(spec=Signal)]
+        interval = Interval(0, 10)
+
+        result = model.simulate(static_inputs, signals, interval)
+
+        for signal in signals:
+            self.assertEqual(signal.at_times.call_count, 1)
+
+        func.assert_called_once()
+
+        fn_static_inputs, signal_times, signal_traces = func.call_args.args
+        expected_n_points = int(interval.length / sampling_interval)
+
+        self.assertEqual(result, func.return_value)
+
+        self.assertListEqual(static_inputs, fn_static_inputs)
+
+        self.assertIsInstance(signal_times, ndarray)
+        self.assertEqual(signal_times.ndim, 1)
+        self.assertEqual(signal_times.size, expected_n_points)
+
+        self.assertIsInstance(signal_traces, ndarray)
+        self.assertEqual(signal_traces.shape[0], len(signals))
+
+        for signal, signal_trace in zip(signals, signal_traces):
+            self.assertEqual(signal_trace, signal.at_times.return_value)
 
 
 class BlackboxDecoratorTestCase(TestCase):
     def test_with_args(self) -> None:
-        @models.blackbox(sampling_interval=0.2)
-        def dummy(
-            params: models.StaticParameters, times: models.SignalTimes, signals: models.SignalValues
-        ) -> models.ModelResult[Any]:
-            pass
+        @blackbox(sampling_interval=0.2)
+        def dummy(static: StaticInput, times: SignalTimes, signals: SignalValues) -> AnyData:
+            ...
 
-        self.assertIsInstance(dummy, models.Blackbox)
+        self.assertIsInstance(dummy, Blackbox)
         self.assertEqual(dummy.sampling_interval, 0.2)
 
     def test_without_args(self) -> None:
-        @models.blackbox
-        def dummy(
-            params: models.StaticParameters, times: models.SignalTimes, signals: models.SignalValues
-        ) -> models.ModelResult[Any]:
-            pass
+        @blackbox
+        def dummy(static: StaticInput, times: SignalTimes, signals: SignalValues) -> AnyData:
+            ...
 
-        self.assertIsInstance(dummy, models.Blackbox)
+        self.assertIsInstance(dummy, Blackbox)
         self.assertEqual(dummy.sampling_interval, 0.1)
 
 
-class SimulationResultTestCase(TestCase):
-    def setUp(self) -> None:
-        self.timestamps = np.array([1, 2, 3])
-        self.trajectories1d = np.array([1, 2, 3])
-        self.trajectories2d = np.array([self.trajectories1d, self.trajectories1d])
+class ODEDecoratorTestCase(TestCase):
+    def test_with_args(self) -> None:
+        @ode(method="RK23")
+        def dummy(time: float, state: State, signals: NDArray[Any]) -> State:
+            ...
 
-    def test_shape_validation(self) -> None:
-        self.assertIsInstance(
-            models.SimulationResult(self.trajectories1d, self.timestamps), models.SimulationResult
-        )
-        self.assertIsInstance(
-            models.SimulationResult(self.trajectories2d, self.timestamps), models.SimulationResult
-        )
-        self.assertIsInstance(
-            models.SimulationResult(self.trajectories2d.T, self.timestamps), models.SimulationResult
-        )
+        self.assertIsInstance(dummy, ODE)
+        self.assertEqual(dummy.method, "RK23")
 
-        with self.assertRaises(ValueError):
-            models.SimulationResult(
-                np.array([self.trajectories2d, self.trajectories2d]), self.timestamps
-            )
-            models.SimulationResult(
-                self.trajectories2d, np.array([self.timestamps, self.timestamps])
-            )
-            models.SimulationResult(self.trajectories2d, np.array([1, 2, 3, 4]))
+    def test_without_args(self) -> None:
+        @ode
+        def dummy(time: float, state: State, signals: NDArray[Any]) -> State:
+            ...
 
-    def test_without_extra(self) -> None:
-        self.assertIsInstance(
-            models.SimulationResult(self.trajectories2d, self.timestamps), models.SimulationResult
-        )
-
-    def test_with_extra(self) -> None:
-        self.assertIsInstance(
-            models.SimulationResult(self.trajectories2d, self.timestamps, "foo"),
-            models.SimulationResult,
-        )
+        self.assertIsInstance(dummy, ODE)
+        self.assertEqual(dummy.method, "RK45")
