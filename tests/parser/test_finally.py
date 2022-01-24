3649febd790594c7c5efe69d597ab2038119a6cf
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-01-24 15:33:29 -0700

    test: Fix missing attribute error in test case for Finally objects

--- tests/parser/test_finally.py
+++ tests/parser/test_finally.py
@@ -36,4 +36,4 @@ class FinallyTestCase(ParserTestCase):
         self.assert_is_finally(parse(r"F(1, 1000) (pred1 && pred2)", self._preds))
 
     def test_finally_with_or(self) -> None:
-        self.assert_is_finally(self._parser.parse(r"<>(25, 3000) (pred2 | pred3)", self._preds))
+        self.assert_is_finally(parse(r"<>(25, 3000) (pred2 | pred3)", self._preds))
