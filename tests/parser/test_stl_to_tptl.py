f6aed80d30e3c2f095dabc209b024e5570cd4ad9
Jacob Anderson <andersonjwan@gmail.com>
2022-04-12 13:30:38 -0700

    tests: Add skeleton tests of STL to TPTL translation scheme

---
+++ tests/parser/test_stl_to_tptl.py
@@ -0,0 +1,69 @@
+from unittest import TestCase
+
+from staliro.parser import translate
+
+class StlTptlTranslationTestCase(TestCase):
+    """Test the equivalency translations from STL to TPTL.
+    """
+
+    def setUp(self) -> None:
+        """Prepare reusable items.
+        """
+
+        pass
+
+
+    def test_translate_negation(self) -> None:
+        pass
+
+
+    def test_translate_next(self) -> None:
+        pass
+
+
+    def test_translate_eventually(self) -> None:
+        pass
+
+
+    def test_translate_always(self) -> None:
+        pass
+
+
+    def test_translate_until(self) -> None:
+        pass
+
+
+    def test_translate_release(self) -> None:
+        pass
+
+
+    def test_translate_implication(self) -> None:
+        pass
+
+
+    def test_translate_equivalence(self) -> None:
+        pass
+
+
+    def test_translate_conjunction(self) -> None:
+        pass
+
+
+    def test_translate_disjunction(self) -> None:
+        pass
+
+
+    def test_translate_unbounded_predicate(self) -> None:
+        pass
+
+
+    def test_translate_bounded_predicate(self) -> None:
+        pass
+
+
+    def test_translate_complex_requirement_01(self) -> None:
+        pass
+
+
+    def test_translate_complex_requirement_01(self) -> None:
+        pass
