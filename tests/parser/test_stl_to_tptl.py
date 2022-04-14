36a3b576362e5500132a3786060614a6d4b624ed
Jacob Anderson <andersonjwan@gmail.com>
2022-04-14 12:54:21 -0700

    tests: Add tests for bounded temporal operators

--- tests/parser/test_stl_to_tptl.py
+++ tests/parser/test_stl_to_tptl.py
@@ -84,17 +84,47 @@ class StlTptlTranslationTestCase(TestCase):
 
         self.assertEqual("a \/ b \/ c", translation)
 
+    def test_translate_bounded_next(self) -> None:
+        source = r"next[0, 10] a"
+        translation = translate(source, TL.STL, TL.TPTL)
+
+        self.assertEqual("@Var_t0 X(({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) /\ a)", translation)
+
+    def test_translate_bounded_eventually(self) -> None:
+        source = r"eventually[3, 5] a"
+        translation = translate(source, TL.STL, TL.TPTL)
+
+        self.assertEqual("@Var_t0 <>(({ Var_t0 >= 3.0 } /\ { Var_t0 <= 5.0 }) /\ a)", translation)
+
+    def test_translate_bounded_always(self) -> None:
+        source = r"always[0, 100] a"
+        translation = translate(source, TL.STL, TL.TPTL)
+
+        self.assertEqual("@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 100.0 }) -> a)", translation)
+
+    def test_translate_bounded_until(self) -> None:
+        source = r"a until[0, 2] b"
+        translation = translate(source, TL.STL, TL.TPTL)
+
+        self.assertEqual("@Var_t0 ( a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 2.0 }) /\ b))", translation)
+
+    def test_translate_bounded_release(self) -> None:
+        source = r"a release[0, 5] b"
+        translation = translate(source, TL.STL, TL.TPTL)
+
+        self.assertEqual("!(@Var_t0 (!a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 5.0 }) /\ !b)))", translation)
+
     def test_translate_unbounded_predicate(self) -> None:
         source = r"globally (eventually a) /\ (eventually b)"
         translation = translate(source, TL.STL, TL.TPTL)
 
         self.assertEqual("[] ( <> a ) /\ ( <> b )", translation)
 
-    # def test_translate_bounded_predicate(self) -> None:
-    #     source = r"always[0, 10] (a /\ (eventually[0, 3] b))"
-    #     translation = translate(source, TL.STL, TL.TPTL)
+    def test_translate_bounded_predicate(self) -> None:
+        source = r"always[0, 10] (a /\ (eventually[0, 3] b))"
+        translation = translate(source, TL.STL, TL.TPTL)
 
-    #     self.assertEqual("@Var_t1 (({ Var_t1 >= 0 } /\ { Var_t1 <= 10}) -> (a /\ (@Var_t2 <>(({ Var_t2 >= 0 } /\ { Var_t2 <= 3 }) /\ b))))", translation)
+        self.assertEqual("@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) -> ( a /\ ( @Var_t1 <>(({ Var_t1 >= 0.0 } /\ { Var_t1 <= 3.0 }) /\ b) ) ))", translation)
 
     # def test_translate_complex_requirement_01(self) -> None:
     #     pass
