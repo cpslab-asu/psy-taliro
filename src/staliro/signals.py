c17ebb2fc2b62acda8a0d7897c5986119509fc4f
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-05-16 19:18:57 -0700

    feat: Create delayed signal combinator to start a signal after a delay
    The delayed combinator will return zero when called with a time value before the delay cutoff

--- src/staliro/signals.py
+++ src/staliro/signals.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import List, Sequence, cast
 
+import numpy as np
 from scipy.interpolate import PchipInterpolator, interp1d
 
-from .core.signal import Signal
+from .core.signal import Signal, SignalFactory
 
 
 class Pchip(Signal):
@@ -50,3 +51,31 @@ class PiecewiseConstant(Signal):
 
 def piecewise_constant(times: Sequence[float], signal_values: Sequence[float]) -> PiecewiseConstant:
     return PiecewiseConstant(interp1d(times, signal_values, kind="zero", fill_value="extrapolate"))
+
+
+class Delayed(Signal):
+    def __init__(self, signal: Signal, cutoff: int):
+        self.signal = signal
+        self.cutoff = cutoff
+
+    def at_time(self, t: float) -> float:
+        if t < self.cutoff:
+            return 0.0
+
+        return self.signal.at_time(t)
+
+    def at_times(self, ts: Sequence[float]) -> List[float]:
+        return [self.at_time(t) for t in ts]
+
+
+def delayed(signal_factory: SignalFactory, *, delay: int) -> SignalFactory:
+    def factory(times: Sequence[float], signal_values: Sequence[float]) -> Signal:
+        stop_time = max(times)
+        new_times = np.linspace(
+            start=delay, stop=stop_time, num=len(signal_values), dtype=np.float64
+        )
+        signal = signal_factory(new_times.tolist(), signal_values)
+
+        return Delayed(signal, delay)
+
+    return factory
