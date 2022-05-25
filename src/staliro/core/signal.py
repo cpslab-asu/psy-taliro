87d2fa4f9155a74b326ec3a0c021838776672385
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-05-25 16:04:57 -0700

    refactor: Make Signal an ABC to make inheritance more explicit. Provide a default implementation of the at_times method

--- src/staliro/core/signal.py
+++ src/staliro/core/signal.py
@@ -1,24 +1,25 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from typing import List, Sequence
 
-from typing_extensions import Protocol, runtime_checkable
+from typing_extensions import Protocol
 
 
-@runtime_checkable
-class Signal(Protocol):
+class Signal(ABC):
     """Representation of a time-varying input to a system."""
 
+    @abstractmethod
     def at_time(self, time: float) -> float:
         """Get the value of the signal at the specified time."""
         ...
 
     def at_times(self, times: Sequence[float]) -> List[float]:
         """Get the value of the signal at each specified time."""
-        ...
+
+        return [self.at_time(time) for time in times]
 
 
-@runtime_checkable
 class SignalFactory(Protocol):
     """Construct a Signal using the provided times and signal values."""
 
