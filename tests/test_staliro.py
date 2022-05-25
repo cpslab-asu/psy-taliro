5f311a96f784520a31b134ea5eb98526a08f53e3
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-05-25 16:10:37 -0700

    test: Update staliro test cases that use SignalOptions

--- tests/test_staliro.py
+++ tests/test_staliro.py
@@ -20,7 +20,7 @@ class StaliroTestCase(TestCase):
         staliro(model, specification, optimizer, options)
 
         scenario_mock.assert_called_once()
-        args, kwargs = scenario_mock.call_args
+        _, kwargs = scenario_mock.call_args
 
         self.assertEqual(kwargs["static_parameter_range"], slice(0, 3, 1))
 
@@ -33,13 +33,13 @@ class StaliroTestCase(TestCase):
         optimizer = NonCallableMock(spec=Optimizer)
 
         static_inputs = [(0, 1), (10, 12), (3, 4)]
-        signals = [SignalOptions((0, 100), control_points=7)]
+        signals = [SignalOptions(control_points=[(0, 100)] * 7)]
         options = Options(runs=1, iterations=10, static_parameters=static_inputs, signals=signals)
 
         staliro(model, specification, optimizer, options)
 
         scenario_mock.assert_called_once()
-        args, kwargs = scenario_mock.call_args
+        _, kwargs = scenario_mock.call_args
         signal_params: list[SignalParameters] = kwargs["signal_parameters"]
 
         self.assertEqual(len(signal_params), len(signals))
