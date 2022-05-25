7fedb5699eec3d09ec2e03b9a4637e1c6a0d7093
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-05-25 16:09:26 -0700

    test: Update options test cases to reflect changes to the SignalOptions class

--- tests/test_options.py
+++ tests/test_options.py
@@ -8,10 +8,9 @@ from staliro.options import Options, OptionsError, SignalOptions
 
 class SignalOptionsTestCase(TestCase):
     def test_bound_conversion(self) -> None:
-        SignalOptions(Interval(0, 1))
-        SignalOptions([1, 2])
-        SignalOptions((1.0, 2.0))
-        SignalOptions(np.array([1, 2], dtype=np.float32))
+        SignalOptions([[1, 2]])
+        SignalOptions([(1.0, 2.0)])
+        SignalOptions([np.array([1, 2], dtype=np.float32)])
 
         with self.assertRaises(TypeError):
             SignalOptions({1, 2})  # type: ignore
@@ -19,72 +18,59 @@ class SignalOptionsTestCase(TestCase):
         with self.assertRaises(TypeError):
             SignalOptions({1: 3, 2: 4})  # type: ignore
 
-        with self.assertRaises(TypeError):
+        with self.assertRaises(Exception):
             SignalOptions("a")  # type: ignore
 
         with self.assertRaises(TypeError):
             SignalOptions(1)  # type: ignore
 
-    def test_control_points(self) -> None:
-        i = Interval(0, 1)
-
-        SignalOptions(i, control_points=10)
-        SignalOptions(i, control_points=10.0)
-
-        with self.assertRaises(OptionsError):
-            SignalOptions(i, control_points="10")  # type: ignore
-
-        with self.assertRaises(OptionsError):
-            SignalOptions(i, control_points=[1, 2])  # type: ignore
-
-        with self.assertRaises(OptionsError):
-            SignalOptions(i, control_points={1, 2})  # type: ignore
-
     def test_signal_times(self) -> None:
-        i = Interval(0, 1)
+        i = [(0, 1)]
 
         SignalOptions(i, signal_times=np.linspace(0, 10, 10))
-        SignalOptions(i, signal_times=[1, 2, 3, 4])
-        SignalOptions(i, signal_times=(1, 2, 3, 4))
+        SignalOptions(i, signal_times=[1.0, 2.0, 3.0, 4.0])
+        SignalOptions(i, signal_times=(1.0, 2.0, 3.0, 4.0))
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             SignalOptions(i, signal_times="1234")  # type: ignore
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             SignalOptions(i, signal_times={1, 2, 3, 4})  # type: ignore
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             SignalOptions(i, signal_times={1: 2, 3: 4})  # type: ignore
 
 
 class OptionsTestCast(TestCase):
     def test_static_parameters(self) -> None:
-        Options([[1, 2], (3, 4), Interval(5, 6)])
-        Options(([1, 2], (3, 4), Interval(5, 6)))
+        Options([[1, 2], (3, 4)])
+        Options(([1, 2], (3, 4)))
 
         with self.assertRaises(TypeError):
             Options([{1, 2}, {1: 2, 3: 4}])  # type: ignore
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             Options({1, (3, 4), Interval(5, 6)})  # type: ignore
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             Options({1: [1, 2], 2: (3, 4), 3: Interval(5, 6)})  # type: ignore
 
     def test_signals(self) -> None:
-        Options(signals=[SignalOptions(Interval(0, 1))])
+        Options(signals=[SignalOptions([(0, 1)])])
 
         with self.assertRaises(TypeError):
             Options(signals=[Interval(0, 1)])  # type: ignore
 
     def test_runs(self) -> None:
         Options(runs=1)
-        Options(runs=1.0)
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
+            Options(runs=1.0)  # type: ignore
+
+        with self.assertRaises(TypeError):
             Options(runs="a")  # type: ignore
 
-        with self.assertRaises(OptionsError):
+        with self.assertRaises(TypeError):
             Options(runs=[1, 2])  # type: ignore
 
     def test_parallelization(self) -> None:
