7377cac7b987636aed6abc0bd83914d9221e0e92
Jacob Anderson <andersonjwan@gmail.com>
2022-08-13 11:12:38 -0400

    refactor: Add type annotation

--- tests/test_specifications.py
+++ tests/test_specifications.py
@@ -121,7 +121,7 @@ class SpecificationTestCase(TestCase):
     )
     def test_tp_taliro_specification_hybrid(self) -> None:
         requirement = "globally (p1 and p2)"
-        predicates = [  # FIXME: add type annotation
+        predicates: List[TaliroPredicate] = [
             {"name": "p1", "a": -1.0, "b": 0.0, "l": 1},
             {"name": "p2", "a": -1.0, "b": -5.0, "l": 1},
             {"name": "p3", "a": 1.0, "b": 30.0, "l": 2},
