37fe5842ecbee0cc1a94a5bb04616a49ff4b6fa6
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-01-04 17:10:45 -0700

    refactor: Remove tltk_mtl and py-taliro dependencies
    Update the TPTL to STL translation layer tests to avoid running if the
    parser module is not importable - i.e. when tltk-mtl is not installed

--- tests/parser/test_stl_to_tptl.py
+++ tests/parser/test_stl_to_tptl.py
@@ -1,8 +1,17 @@
-from unittest import TestCase
+from unittest import TestCase, skipIf
 
-from staliro.parser import TemporalLogic, translate
+try:
+    from staliro.parser import TemporalLogic, translate
+except ImportError:
+    _can_parse = False
+else:
+    _can_parse = True
 
 
+@skipIf(
+    _can_parse is False,
+    "Translation test case is not available without parsing functionality",
+)
 class StlTptlTranslationTestCase(TestCase):
     """Test the equivalency translations from STL to TPTL."""
 
@@ -75,38 +84,38 @@ class StlTptlTranslationTestCase(TestCase):
         source = r"a and b and c"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("a /\ b /\ c", translation)
+        self.assertEqual(r"a /\ b /\ c", translation)
 
     def test_translate_disjunction(self) -> None:
         source = r"a or b or c"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("a \/ b \/ c", translation)
+        self.assertEqual(r"a \/ b \/ c", translation)
 
     def test_translate_bounded_next(self) -> None:
         source = r"next[0, 10] a"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("@Var_t0 X(({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) /\ a)", translation)
+        self.assertEqual(r"@Var_t0 X(({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) /\ a)", translation)
 
     def test_translate_bounded_eventually(self) -> None:
         source = r"eventually[3, 5] a"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("@Var_t0 <>(({ Var_t0 >= 3.0 } /\ { Var_t0 <= 5.0 }) /\ a)", translation)
+        self.assertEqual(r"@Var_t0 <>(({ Var_t0 >= 3.0 } /\ { Var_t0 <= 5.0 }) /\ a)", translation)
 
     def test_translate_bounded_always(self) -> None:
         source = r"always[0, 100] a"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 100.0 }) -> a)", translation)
+        self.assertEqual(r"@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 100.0 }) -> a)", translation)
 
     def test_translate_bounded_until(self) -> None:
         source = r"a until[0, 2] b"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
         self.assertEqual(
-            "@Var_t0 ( a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 2.0 }) /\ b))", translation
+            r"@Var_t0 ( a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 2.0 }) /\ b))", translation
         )
 
     def test_translate_bounded_release(self) -> None:
@@ -114,20 +123,20 @@ class StlTptlTranslationTestCase(TestCase):
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
         self.assertEqual(
-            "!(@Var_t0 (!a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 5.0 }) /\ !b)))", translation
+            r"!(@Var_t0 (!a U (({ Var_t0 >= 0.0 } /\ { Var_t0 <= 5.0 }) /\ !b)))", translation
         )
 
     def test_translate_predicate(self) -> None:
         source = r"globally (eventually a) /\ (eventually b)"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
-        self.assertEqual("[] ( <> a ) /\ ( <> b )", translation)
+        self.assertEqual(r"[] ( <> a ) /\ ( <> b )", translation)
 
     def test_translate_bounded_predicate(self) -> None:
         source = r"always[0, 10] (a /\ (eventually[0, 3] b))"
         translation = translate(source, TemporalLogic.STL, TemporalLogic.TPTL)
 
         self.assertEqual(
-            "@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) -> ( a /\ ( @Var_t1 <>(({ Var_t1 >= 0.0 } /\ { Var_t1 <= 3.0 }) /\ b) ) ))",
+            r"@Var_t0 [](({ Var_t0 >= 0.0 } /\ { Var_t0 <= 10.0 }) -> ( a /\ ( @Var_t1 <>(({ Var_t1 >= 0.0 } /\ { Var_t1 <= 3.0 }) /\ b) ) ))",
             translation,
         )
