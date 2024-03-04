f58895bada4f327cb83f72c21bf45b1897b9acc0
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-03-04 14:25:17 -0700

    refactor: Update top-level re-exports

--- src/staliro/__init__.py
+++ src/staliro/__init__.py
@@ -1,19 +1,25 @@
 from .cost_func import CostFunc, Result, Sample, SampleLike, costfunc
 from .models import Blackbox, Trace, blackbox, model, ode
-from .options import Options
+from .options import SignalOptions, TestOptions
+from .signals import Signal
+from .specifications import Specification, specification
 from .tests import staliro
 
 __all__ = [
     "CostFunc",
-    "Options",
     "Result",
     "Sample",
     "SampleLike",
+    "Signal",
+    "SignalOptions",
+    "Specification",
+    "TestOptions",
     "Trace",
     "Blackbox",
     "blackbox",
     "costfunc",
     "model",
     "ode",
+    "specification",
     "staliro",
 ]
