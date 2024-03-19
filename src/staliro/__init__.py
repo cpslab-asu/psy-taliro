22be52b956e7210d1c997ec2e2340678a8a44c92
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-03-19 09:16:48 -0700

    feat: Export Evaluation and Run from staliro package

--- src/staliro/__init__.py
+++ src/staliro/__init__.py
@@ -1,15 +1,17 @@
 from .cost_func import CostFunc, Result, Sample, SampleLike, costfunc
 from .models import Trace
 from .options import TestOptions
-from .signals import Interval, Signal, SignalInput
+from .signals import Signal, SignalInput
 from .specifications import Specification, specification
-from .tests import staliro
+from .tests import Evaluation, Run, staliro
 
 test = staliro
 
 __all__ = [
+    "Evaluation",
     "CostFunc",
     "Result",
+    "Run",
     "Sample",
     "SampleLike",
     "Signal",
